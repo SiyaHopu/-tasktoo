15f91fd3fa2bf22658c1fa51ce46b4fd987e7d8a
SiyaHopu <113137595+SiyaHopu@users.noreply.github.com>
2024-05-22 10:55:47 +0200

    Added functionality for user specific output

--- XMLtoJAVA.java
+++ XMLtoJAVA.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 import javax.xml.bind.*;
 import java.io.File;
 
 public class XMLToJava {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What field do you require: ");
+        System.out.print("i.e Name, Zip, Region, Country, Address and List.");
+        String input = scanner.nextLine(); // Reads a line of text
+
         try {
             File file = new File("C:\Users\E&T Comp's Lap\-tasktoo\Records.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(Records.class);
@@ -11,12 +18,24 @@ public class XMLToJava {
             Records records = (Records) jaxbUnmarshaller.unmarshal(file);
 
             for (Users user :records.getUsers) {
-                System.out.println("User Name: " + user.getName);
-                System.out.println("PostalZip: " + user.getPostalZip);
-                System.out.println("Region: " + user.getRegion);
-                System.out.println("Country: " + user.getCountry);
-                System.out.println("Address: " + user.getAddress);
-                System.out.println("List: " + user.getList);
+                if (input.toLowerCase() == "name") {
+                    System.out.print("Name: " + user.getName());
+                }
+                if (input.toLowerCase() == "zip") {
+                    System.out.print("PostalZip: " + user.getPostalZip());
+                }
+                if (input.toLowerCase() == "region") {
+                    System.out.print("Region: " + user.getRegion());
+                }
+                if (input.toLowerCase() == "country") {
+                    System.out.print("Country: " + user.getCountry());
+                }
+                if (input.toLowerCase() == "address") {
+                    System.out.print("Address: " + user.getAddress());
+                }
+                elif (input.toLowerCase() == "list") {
+                    System.out.print("List: " + user.getList());
+                }
             }
         } catch (JAXBException e) {
             e.printStackTrace();
